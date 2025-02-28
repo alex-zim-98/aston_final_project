b9fa5a109ed8146bfc90734255f28fae33e3ef89
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-02-28 19:56:01 +0000

    View binding

---
+++ app/src/main/java/com/example/aston_final_project/SavedFragment.kt
@@ -0,0 +1,33 @@
+package com.example.aston_final_project
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.aston_final_project.databinding.FragmentSavedBinding
+
+class SavedFragment : Fragment() {
+
+    private var _binding: FragmentSavedBinding? = null
+    private val binding
+        get() = _binding ?: throw throw IllegalStateException("Binding is accessed after destroy")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSavedBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as MainActivity).binding.customToolbar
+    }
+
+    companion object {
+        fun newInstance() =
+            SavedFragment()
+    }
+}
