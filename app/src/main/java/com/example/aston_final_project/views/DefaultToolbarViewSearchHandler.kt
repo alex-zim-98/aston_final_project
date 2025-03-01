bdfaa2eee00e69d72203b048b2a82df575bda6ec
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-03-01 19:17:24 +0000

    aoo component

---
+++ app/src/main/java/com/example/aston_final_project/views/DefaultToolbarViewSearchHandler.kt
@@ -0,0 +1,5 @@
+package com.example.aston_final_project.views
+
+interface DefaultToolbarViewSearchHandler {
+    fun onViewSearchClicked(action: () -> Unit)
+}
---
+++ app/src/main/java/com/example/aston_final_project/views/ToolbarController.kt
@@ -0,0 +1,7 @@
+package com.example.aston_final_project.views
+
+import com.example.aston_final_project.Status
+
+interface ToolbarController {
+    fun setToolbarStatus(status: Status)
+}
