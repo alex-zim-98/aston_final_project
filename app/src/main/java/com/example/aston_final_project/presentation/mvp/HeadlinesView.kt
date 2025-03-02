36b6d65d932bb74f87554777912d30affa3093c8
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-03-02 20:25:25 +0000

    created presenter

---
+++ app/src/main/java/com/example/aston_final_project/presentation/mvp/HeadlinesView.kt
@@ -0,0 +1,12 @@
+package com.example.aston_final_project.presentation.mvp
+
+import com.example.aston_final_project.domain.entity.Article
+import moxy.MvpView
+import moxy.viewstate.strategy.OneExecutionStateStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(value = OneExecutionStateStrategy::class)
+interface HeadlinesView : MvpView {
+    fun getHeadlinesList(list: List<Article>)
+    fun showError(error: String)
+}
