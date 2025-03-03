097f848abfce8e796bcb9d1aca99f6d9316187ce
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-03-03 15:38:13 +0000

    put the usecases logic into a separate class

--- app/src/main/java/com/example/aston_final_project/presentation/mvp/HeadlinesPresenter.kt
+++ app/src/main/java/com/example/aston_final_project/presentation/mvp/HeadlinesPresenter.kt
@@ -2,6 +2,8 @@ package com.example.aston_final_project.presentation.mvp
 
 import com.example.aston_final_project.app.network.NetworkManager
 import com.example.aston_final_project.app.util.checkInternetConnection
+import com.example.aston_final_project.domain.entity.Article
+import com.example.aston_final_project.domain.repository.LocalRepository
 import com.example.aston_final_project.domain.repository.RemoteRepository
 import com.example.aston_final_project.presentation.mapper.RequestMapper
 import com.example.aston_final_project.presentation.search.HeadlinesSearchSearchMode
@@ -10,50 +12,60 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import moxy.MvpPresenter
 import javax.inject.Inject
 
 class HeadlinesPresenter @Inject constructor(
     remoteRepository: RemoteRepository,
+    localRepository: LocalRepository,
     private val networkManager: NetworkManager,
     requestMapper: RequestMapper
 ) : MvpPresenter<HeadlinesView>() {
     private var isLoading = false
     private val compositeDisposable = CompositeDisposable()
-    private val mutex = Mutex()
     private val headlinesEverythingList = HeadlinesSearchSearchMode(
         remoteRepository,
+        localRepository,
         requestMapper
     )
 
     fun searchNews() {
         if (isLoading) return
+
         isLoading = true
+
         networkManager.checkInternetConnection(
             doOnSuccess = { searchByInternet() },
             doOnFail = {}
         )
-        searchByInternet()
     }
 
     private fun searchByInternet() {
-        CoroutineScope(Dispatchers.IO).launch {
-            mutex.withLock {
-                val disposable = headlinesEverythingList.fetchDataList()
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                        {
-                            viewState.getHeadlinesList(it)
-                        },
-                        { viewState.showError(it.toString()) }
-                    )
-                compositeDisposable.add(disposable)
-                isLoading = false
+        val disposable = headlinesEverythingList.fetchDataList()
+            .doOnSuccess { response ->
+                CoroutineScope(Dispatchers.IO).launch {
+                    saveToCash(response)
+                }
             }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { response ->
+                    viewState.getHeadlinesList(response)
+                },
+                { viewState.showError(it.toString()) }
+            )
+        compositeDisposable.add(disposable)
+        isLoading = false
+    }
+
+    private suspend fun saveToCash(response: List<Article>) {
+        withContext(Dispatchers.IO) {
+            headlinesEverythingList.clearNewsEverything()
+            headlinesEverythingList.insertNewsEverything(response)
         }
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
