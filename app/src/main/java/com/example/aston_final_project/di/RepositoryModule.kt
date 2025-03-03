a24a455da8c6baf4849c52849d6c5ca9d70b5105
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-03-03 15:45:34 +0000

    bind local repository

--- app/src/main/java/com/example/aston_final_project/di/RepositoryModule.kt
+++ app/src/main/java/com/example/aston_final_project/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.example.aston_final_project.di
 
+import com.example.aston_final_project.data.repository.LocalRepositoryImpl
 import com.example.aston_final_project.data.repository.RemoteRepositoryImpl
+import com.example.aston_final_project.domain.repository.LocalRepository
 import com.example.aston_final_project.domain.repository.RemoteRepository
 import dagger.Binds
 import dagger.Module
@@ -9,4 +11,7 @@ import dagger.Module
 interface RepositoryModule {
     @Binds
     fun bindRemoteRepository(repository: RemoteRepositoryImpl): RemoteRepository
+
+    @Binds
+    fun bindLocalRepository(repository: LocalRepositoryImpl): LocalRepository
 }
