87cd1be6c49eef95f386a6e2104240536b66515a
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-03-03 15:35:12 +0000

    remove import Observable to Single

--- app/src/main/java/com/example/aston_final_project/domain/repository/RemoteRepository.kt
+++ app/src/main/java/com/example/aston_final_project/domain/repository/RemoteRepository.kt
@@ -1,9 +1,9 @@
 package com.example.aston_final_project.domain.repository
 
 import com.example.aston_final_project.domain.entity.Article
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 interface RemoteRepository {
-    fun getTopHeadlines(params: Map<String, String>): Observable<List<Article>>
-    fun searchNews(params: Map<String, String>): Observable<List<Article>>
+    fun getTopHeadlines(params: Map<String, String>): Single<List<Article>>
+    fun searchNews(params: Map<String, String>): Single<List<Article>>
 }
