731801b262436ef16ae18aa41fba52bf0d93d5b3
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2025-03-05 17:26:43 +0000

    remove usual variables and change to PresenterState

---
+++ app/src/main/java/com/example/aston_final_project/presentation/mvp/PresenterState.kt
@@ -0,0 +1,9 @@
+package com.example.aston_final_project.presentation.mvp
+
+import com.example.aston_final_project.domain.entity.Article
+
+data class PresenterState (
+    val isLoading: Boolean,
+    val listTopHeadlines: List<Article>,
+    val numberPage: Int
+)
